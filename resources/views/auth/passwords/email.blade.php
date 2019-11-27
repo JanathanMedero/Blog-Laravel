2c559143c501469d173964d40de6f1c7763fabe0
Janathan Medero <medero.janathan@gmail.com>
2019-11-26 19:31:39 -0600

    Design reset password view

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,41 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
+            <div class="card card-background shadow-sm">
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="form-group row d-flex align-items-center">
+                            <div class="col-md-3">
+                                <img src="{{ asset('images/password.svg') }}" alt="forgot password" class="img-fluid">
                             </div>
-                        </div>
+                            <div class="col-md-9">
+                                <div class="row">
+                                    <div class="col-md-12 form-group">
+                                        <input id="email" type="email" class="form-control border-0 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required placeholder="Ingresa el correo electrónico">
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-12 form-group mb-0">
+                                        <button type="submit" class="btn btn-primary btn-block">
+                                            Enviar correo de recuperación
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </form>
